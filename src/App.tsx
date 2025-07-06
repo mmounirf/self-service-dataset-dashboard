ebc14ede29aa7141c8c3886e6bb7e48037d797b3
Mou <m.mounir.f@gmail.com>
2025-07-06 20:35:44 +0300

    branding

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Button, MantineProvider } from "@mantine/core";
+import { IconEyeBolt } from "@tabler/icons-react";
+import theme from "./theme";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
+		<MantineProvider theme={theme}>
+			<Button size="md" leftSection={<IconEyeBolt />}>
+				Click Me
+			</Button>
 			<div>
-				<a href="https://vite.dev" target="_blank" rel="noopener">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noopener">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+				<h1>Self-service Dataset Dashboard</h1>
+				<p>Welcome to the Self-service Dataset Dashboard!</p>
+				<p>This is a prototype UI built with Mantine.</p>
+				<p>Feel free to explore and customize it further.</p>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</MantineProvider>
 	);
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,21 @@
+import { createTheme } from "@mantine/core";
+
+const theme = createTheme({
+	primaryColor: "emerald",
+	colors: {
+		emerald: [
+			"#d1fae5",
+			"#a7f3d0",
+			"#6ee7b7",
+			"#34d399",
+			"#10b981",
+			"#059669",
+			"#047857",
+			"#065f46",
+			"#064e3b",
+			"#022c22",
+		],
+	},
+});
+
+export default theme;
